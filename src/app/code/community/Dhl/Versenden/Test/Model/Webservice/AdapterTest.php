bb494b38202012683a059567227afde44097de30
Christoph Aßmann <christoph.assmann@netresearch.de>
2016-07-25 18:36:22 +0200

    DHLGKP-17: generate soap classes

---
+++ src/app/code/community/Dhl/Versenden/Test/Model/Webservice/AdapterTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Dhl Versenden
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to
+ * newer versions in the future.
+ *
+ * PHP version 5
+ *
+ * @category  Dhl
+ * @package   Dhl_Versenden
+ * @author    Christoph Aßmann <christoph.assmann@netresearch.de>
+ * @copyright 2016 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+
+/**
+ * Dhl_Versenden_Test_Model_Webservice_AdapterTest
+ *
+ * @category Dhl
+ * @package  Dhl_Versenden
+ * @author   Christoph Aßmann <christoph.assmann@netresearch.de>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link     http://www.netresearch.de/
+ */
+class Dhl_Versenden_Test_Model_Webservice_AdapterTest
+    extends EcomDev_PHPUnit_Test_Case
+{
+    /**
+     * @test
+     */
+    public function getVersion()
+    {
+        $client = new \Dhl\Bcs\Api\GVAPI_2_0_de();
+        $version = new \Dhl\Bcs\Api\Version('2', '1', null);
+        $response = $client->getVersion($version);
+        print_r($response);
+    }
+}
