3cc69dc81d19c2fdb85ff7f267fbb3b5cbd39e45
Christoph Aßmann <christoph.assmann@netresearch.de>
2016-09-09 14:07:07 +0200

    DHLGKP-25: delete label after shipment cancellation

--- src/app/code/community/Dhl/Versenden/Test/Model/Observer/DeleteTrackTest.php
+++ src/app/code/community/Dhl/Versenden/Test/Model/Observer/DeleteTrackTest.php
@@ -47,10 +47,8 @@ class Dhl_Versenden_Test_Model_Observer_DeleteTrackTest
             ->method('deleteShipmentOrder');
         $this->replaceByMock('model', 'dhl_versenden/webservice_gateway_soap', $gatewayMock);
 
-
         $track = Mage::getModel('sales/order_shipment_track');
         $track->setCarrierCode('foo');
-
 
         $observer = new Varien_Event_Observer();
         $observer->setData('track', $track);
@@ -61,6 +59,28 @@ class Dhl_Versenden_Test_Model_Observer_DeleteTrackTest
 
     /**
      * @test
+     */
+    public function deleteShippingLabelNoLabel()
+    {
+        $gatewayMock = $this->getModelMock('dhl_versenden/webservice_gateway_soap', array('deleteShipmentOrder'));
+        $gatewayMock
+            ->expects($this->never())
+            ->method('deleteShipmentOrder');
+        $this->replaceByMock('model', 'dhl_versenden/webservice_gateway_soap', $gatewayMock);
+
+        $track = Mage::getModel('sales/order_shipment_track');
+        $track->setCarrierCode(Dhl_Versenden_Model_Shipping_Carrier_Versenden::CODE);
+
+        $observer = new Varien_Event_Observer();
+        $observer->setData('track', $track);
+
+        $dhlObserver = new Dhl_Versenden_Model_Observer();
+        $dhlObserver->deleteShippingLabel($observer);
+    }
+
+    /**
+     * @test
+     * @loadFixture Model_ObserverTest
      * @expectedException Mage_Core_Exception
      */
     public function deleteShippingLabelStatusError()
@@ -80,14 +100,48 @@ class Dhl_Versenden_Test_Model_Observer_DeleteTrackTest
             ->willReturn($response);
         $this->replaceByMock('model', 'dhl_versenden/webservice_gateway_soap', $gatewayMock);
 
+        $shipment = Mage::getModel('sales/order_shipment')->load(100);
         $track = Mage::getModel('sales/order_shipment_track');
         $track->setCarrierCode(Dhl_Versenden_Model_Shipping_Carrier_Versenden::CODE);
-
+        $track->setShipment($shipment);
 
         $observer = new Varien_Event_Observer();
         $observer->setData('track', $track);
 
         $dhlObserver = new Dhl_Versenden_Model_Observer();
         $dhlObserver->deleteShippingLabel($observer);
+    }
+
+    /**
+     * @test
+     * @loadFixture Model_ObserverTest
+     */
+    public function deleteShippingLabelOk()
+    {
+        $response = new Varien_Object();
+        $status = new Dhl\Versenden\Webservice\ResponseData\Status\Response('0', 'ok', '');
+        $response->setData('status', $status);
+
+        $gatewayMock = $this->getModelMock('dhl_versenden/webservice_gateway_soap', array('deleteShipmentOrder'));
+        $gatewayMock
+            ->expects($this->once())
+            ->method('deleteShipmentOrder')
+            ->willReturn($response);
+        $this->replaceByMock('model', 'dhl_versenden/webservice_gateway_soap', $gatewayMock);
+
+        $shipment = Mage::getModel('sales/order_shipment')->load(100);
+        $this->assertNotEmpty($shipment->getData('shipping_label'));
+
+        $track = Mage::getModel('sales/order_shipment_track');
+        $track->setCarrierCode(Dhl_Versenden_Model_Shipping_Carrier_Versenden::CODE);
+        $track->setShipment($shipment);
+
+        $observer = new Varien_Event_Observer();
+        $observer->setData('track', $track);
+
+        $dhlObserver = new Dhl_Versenden_Model_Observer();
+        $dhlObserver->deleteShippingLabel($observer);
+
+        $this->assertEmpty($shipment->getData('shipping_label'));
     }
 }
