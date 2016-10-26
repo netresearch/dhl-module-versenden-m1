dba28107e1785412f02fd06a763fa4f14717a2cc
Pacolu <benjamin.heuer@netresearch.de>
2016-10-26 17:08:11 +0200

    DHLGKP-69: add validation for preferred day, refactor markup for rendering of radio elements

--- src/lib/Dhl/Versenden/Shipment/Service/PreferredDay.php
+++ src/lib/Dhl/Versenden/Shipment/Service/PreferredDay.php
@@ -41,9 +41,9 @@ class PreferredDay extends Type\Radio
     /**
      * DeliveryTimeFrame constructor.
      *
-     * @param string $name
-     * @param bool $isEnabled
-     * @param bool $isSelected
+     * @param string   $name
+     * @param bool     $isEnabled
+     * @param bool     $isSelected
      * @param string[] $options
      */
     public function __construct($name, $isEnabled, $isSelected, $options)
@@ -53,5 +53,40 @@ class PreferredDay extends Type\Radio
         parent::__construct($name, $isEnabled, $isSelected, $options);
     }
 
+    /**
+     * @return string
+     */
+    public function getValueHtml()
+    {
+        $options = $this->getOptions();
+        $values  = array_keys($options);
 
+        $optionsHtml = array_reduce(
+            $values,
+            function($carry, $value) use ($options) {
+                $checked      = ($value == $this->getValue()) ? 'checked="checked"' : '';
+                $disabled     =
+                    (isset($options[$value]['disabled']) && $options[$value]['disabled']) ? 'disabled=disabled' : '';
+                $optionValues = explode('-', $options[$value]['value']);
+                $carry .= sprintf(
+                    '<div>' .
+                    '<input type="radio" name="service_setting[%s]" id="shipment_service_%s" %s %s value="%s">' .
+                    '<label for="shipment_service_%s"><span>%s</span><span>%s</span></label>' .
+                    '</div>',
+                    $this->getCode(),
+                    $this->getCode() . '_' . $value,
+                    $checked,
+                    $disabled,
+                    $value,
+                    $this->getCode() . '_' . $value,
+                    $optionValues[0],
+                    $optionValues[1]
+                );
+
+                return $carry;
+            }
+        );
+
+        return $optionsHtml;
+    }
 }
