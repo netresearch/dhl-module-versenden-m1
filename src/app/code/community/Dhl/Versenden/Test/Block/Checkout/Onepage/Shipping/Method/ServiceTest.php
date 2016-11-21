e2f8869310d46d79844726d6c4a79a8c2ec79e2e
Pacolu <benjamin.heuer@netresearch.de>
2016-11-21 12:19:01 +0100

    DHLGKP: add some Unit tests

--- src/app/code/community/Dhl/Versenden/Test/Block/Checkout/Onepage/Shipping/Method/ServiceTest.php
+++ src/app/code/community/Dhl/Versenden/Test/Block/Checkout/Onepage/Shipping/Method/ServiceTest.php
@@ -115,6 +115,8 @@ class Dhl_Versenden_Test_Block_Checkout_Onepage_Shipping_Method_ServiceTest
         /** @var Dhl_Versenden_Block_Checkout_Onepage_Shipping_Method_Service $block */
         $block = Mage::app()->getLayout()->createBlock(self::BLOCK_ALIAS);
 
+        $this->assertNotEmpty($block->getServiceHint(Service\PreferredDay::CODE));
+        $this->assertNotEmpty($block->getServiceHint(Service\PreferredTime::CODE));
         $this->assertNotEmpty($block->getServiceHint(Service\PreferredLocation::CODE));
         $this->assertNotEmpty($block->getServiceHint(Service\PreferredNeighbour::CODE));
         $this->assertEmpty($block->getServiceHint(Service\ParcelAnnouncement::CODE));
