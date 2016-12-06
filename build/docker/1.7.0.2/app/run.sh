51f94872297ddcfbad26b1ddd4397dcff6d71ed7
Christoph Aßmann <christoph.assmann@netresearch.de>
2016-12-06 12:29:51 +0100

    CI-141: add module requirements

--- build/docker/1.7.0.2/app/run.sh
+++ build/docker/1.7.0.2/app/run.sh
@@ -2,6 +2,6 @@
 set -e
 
 composer config -g repositories.pkgundertest path ${PACKAGE_PATH}
-composer require --prefer-source ${PACKAGE_NAME} @dev
+composer require --ignore-platform-reqs --prefer-source ${PACKAGE_NAME} @dev
 
 exit
