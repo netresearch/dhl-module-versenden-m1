27f4e6a0aef44068f37ce6107eae047b72e4207b
Paul Siedler <paul.siedler@netresearch.de>
2016-12-15 15:51:01 +0100

    NRTECH-2565: wait even longer for the database to be ready

--- build/docker/1.9.3.1/app/run.sh
+++ build/docker/1.9.3.1/app/run.sh
@@ -5,7 +5,7 @@ DATABASE_HOST="$1"
 DATABASE_PORT="$2"
 DATABASE_PASSWORD="$3"
 
-retry -w 30 -s "mysql -e 'SELECT 1' --user=root --host=$DATABASE_HOST --port=$DATABASE_PORT -p$DATABASE_PASSWORD;" "echo 'Database ready'"
+retry -w 30 -n 90 -s "mysql -e 'SELECT 1' --user=root --host=$DATABASE_HOST --port=$DATABASE_PORT -p$DATABASE_PASSWORD;" "echo 'Database ready'"
 
 composer config -g repositories.pkgundertest path ${PACKAGE_PATH}
 composer require --ignore-platform-reqs --prefer-source ${PACKAGE_NAME} @dev
