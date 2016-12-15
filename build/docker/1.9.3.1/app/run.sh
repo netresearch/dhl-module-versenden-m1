d9632941cdab33406bc41fdd927f8c6bd4c32821
Paul Siedler <paul.siedler@netresearch.de>
2016-12-15 13:01:19 +0100

    NRTECH-2565: let app container wait for db initialisation, improve healthcheck

--- build/docker/1.9.3.1/app/run.sh
+++ build/docker/1.9.3.1/app/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
-touch /startup.flag
+
+DATABASE_HOST="$1"
+DATABASE_PORT="$2"
+DATABASE_PASSWORD="$3"
+
+retry -w 30 -s "mysql -e 'SELECT 1' --user=root --host=$DATABASE_HOST --port=$DATABASE_PORT -p$DATABASE_PASSWORD;" "echo 'Database ready'"
+
 composer config -g repositories.pkgundertest path ${PACKAGE_PATH}
 composer require --ignore-platform-reqs --prefer-source ${PACKAGE_NAME} @dev
-unlink /startup.flag
-exit
+touch /ready.flag
