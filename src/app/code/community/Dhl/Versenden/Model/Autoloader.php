f292759fa39f0c3d1e2d35edbb7a24b40a755b2d
Christoph Aßmann <christoph.assmann@netresearch.de>
2017-09-25 12:47:12 +0200

    DHLGKP-129: register autoloader only once

--- src/app/code/community/Dhl/Versenden/Model/Autoloader.php
+++ src/app/code/community/Dhl/Versenden/Model/Autoloader.php
@@ -25,7 +25,19 @@
 class Dhl_Versenden_Model_Autoloader
 {
     /**
+     * Order of autoloaders gets shuffled if the same autoloader is registered
+     * more than once. Remember state to avoid this.
+     *
+     * @var bool
+     */
+    protected $_isRegistered = false;
+
+    /**
      * Register autoloader in order to locate the extension libraries.
+     *
+     * To make sure the autoloader gets registered only once, use registration
+     * with registered check.
+     * @see registerAutoload()
      */
     public static function register()
     {
@@ -45,5 +57,16 @@ class Dhl_Versenden_Model_Autoloader
         );
 
         $autoloader->register();
+    }
+
+    /**
+     * Register autoloader with registered check.
+     */
+    public function registerAutoload()
+    {
+        if (!$this->_isRegistered) {
+            static::register();
+            $this->_isRegistered = true;
+        }
     }
 }
