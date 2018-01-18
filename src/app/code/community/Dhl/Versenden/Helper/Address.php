54c68c93d79feeaf10ebceb9fea2452d5a550546
Andreas Müller <andreas.mueller@netresearch.de>
2018-01-18 11:49:42 +0100

    DHLGKP-155: Fixed Report Errors, added _isAllowed & identical operator

--- src/app/code/community/Dhl/Versenden/Helper/Address.php
+++ src/app/code/community/Dhl/Versenden/Helper/Address.php
@@ -92,12 +92,12 @@ class Dhl_Versenden_Helper_Address extends Mage_Core_Helper_Abstract
             }
         }
 
-        if (stripos($result['street_number'], '/')) {
+        if (stripos($result['street_number'], '/') !== false) {
             list($result['street_number'], $addition) = explode('/', $result['street_number'], 2);
             $result['supplement'] = $addition.$result['supplement'];
             //remove empty spaces if occure
-            $result['street_number'] = str_replace(' ', '',$result['street_number']);
-            $result['supplement'] = str_replace(' ', '',$result['supplement']);
+            $result['street_number'] = str_replace(' ', '', $result['street_number']);
+            $result['supplement'] = str_replace(' ', '', $result['supplement']);
         }
 
         return $result;
