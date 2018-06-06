0360d81a8f46af22aff33ff714941ea4ad49f106
Norman Golatka <norman.golatka@netresearch.de>
2018-06-06 12:23:37 +0200

    OPSDHL-898: allow using placeholders in bank data fields

--- src/app/code/community/Dhl/Versenden/Model/Webservice/Builder/Shipper.php
+++ src/app/code/community/Dhl/Versenden/Model/Webservice/Builder/Shipper.php
@@ -38,6 +38,9 @@ class Dhl_Versenden_Model_Webservice_Builder_Shipper
     /** @var Dhl_Versenden_Model_Config_Shipper */
     protected $config;
 
+    /** @var Mage_Sales_Model_Order_Shipment */
+    protected $shipment;
+
     /**
      * Dhl_Versenden_Model_Webservice_Builder_Shipper constructor.
      * @param Dhl_Versenden_Model_Config[] $args
@@ -61,6 +64,40 @@ class Dhl_Versenden_Model_Webservice_Builder_Shipper
      */
     public function getShipper($store)
     {
-        return $this->config->getShipper($store);
+        $shipper = $this->config->getShipper($store);
+
+        if ($this->shipment != null)
+        {
+            $shipper = new Shipper(
+                $this->config->getAccountSettings(),
+                $this->config->getBankData($store, $this->getBankRefMap()),
+                $this->config->getContact($store),
+                $this->config->getReturnReceiver($store)
+            );
+        }
+        return $shipper;
+    }
+
+     /**
+     * @param Mage_Sales_Model_Order_Shipment $shipment
+     * @return Shipment
+     */
+    public function setShipment(Mage_Sales_Model_Order_Shipment $shipment)
+    {
+        $this->shipment = $shipment;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getBankRefMap()
+    {
+        // Available placeholders for the bank data configuration
+        return array(
+            '%orderId%'      => $this->shipment->getOrder()->getIncrementId(),
+            '%customerName%' => $this->shipment->getOrder()->getCustomerName(),
+            '%customerId%'   => $this->shipment->getOrder()->getCustomerId()
+        );
     }
 }
