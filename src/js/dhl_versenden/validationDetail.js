c8dcdaa463e2d716e627ef541ef4709e9a275d02
Andreas Müller <andreas.mueller@netresearch.de>
2018-07-17 12:26:03 +0200

    DHLGKP-180: add backend validation

---
+++ src/js/dhl_versenden/validationDetail.js
@@ -0,0 +1,10 @@
+Validation.add(
+    'validate-detail',
+    'Your input is invalid',
+    function (v) {
+
+        var exp = /\bPaketbox|\bPackstation|\bPostfach|\bPostfiliale|\bFiliale|\bPostfiliale Direkt|\bFiliale Direkt|\bPaketkasten|\bDHL|\bP-A-C-K-S-T-A-T-I-O-N|\bPaketstation|\bPack Station|\bP.A.C.K.S.T.A.T.I.O.N.|\bPakcstation|\bPaackstation|\bPakstation|\bBackstation|\bBakstation|\bP A C K S T A T I O N|\bWunschfiliale|\bDeutsche Post/g;
+
+        return Validation.get('IsEmpty').test(v) || (!exp.test(v));
+    }
+);
