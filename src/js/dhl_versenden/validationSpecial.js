9b986abf204db9b255e1d422534f2c11b9cd093a
Andreas Müller <andreas.mueller@netresearch.de>
2018-07-17 12:26:03 +0200

    DHLGKP-180: add backend validation

---
+++ src/js/dhl_versenden/validationSpecial.js
@@ -0,0 +1,9 @@
+Validation.add(
+    'validate-special',
+    'Your input is invalid',
+    function (v) {
+        var expSpec = /["+;<>,.']|\\/g;
+
+        return Validation.get('IsEmpty').test(v) || (!expSpec.test(v));
+    }
+);
