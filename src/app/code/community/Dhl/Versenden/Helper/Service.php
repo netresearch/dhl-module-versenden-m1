1e1195164a0de7739b43cd3e0fecb97cc72633e9
Andreas Müller <andreas.mueller@netresearch.de>
2018-07-26 17:17:07 +0200

    DHLGKP-181: frontend enable/disabling

--- src/app/code/community/Dhl/Versenden/Helper/Service.php
+++ src/app/code/community/Dhl/Versenden/Helper/Service.php
@@ -60,18 +60,49 @@ class Dhl_Versenden_Helper_Service extends Mage_Core_Helper_Abstract
     }
 
     /**
+     * @return string
+     */
+    public function setServiceObserver()
+    {
+        if ($this->isLocationAndNeighbourEnabled()) {
+            return 'js/dhl_versenden/excludeService.js';
+        }
+
+        return '';
+    }
+
+    /**
      * @return bool
      */
     public function isLoctionOrneighbourEnabled()
     {
         /**
-         * @var Dhl_Versenden_Model_Config_Service
+         * @var Dhl_Versenden_Model_Config_Service $config
          */
         $config = Mage::getModel('dhl_versenden/config_service');
         $services = $config->getEnabledServices(Mage::app()->getStore()->getStoreId())->getItems();
         $location = \Dhl\Versenden\Bcs\Api\Shipment\Service\PreferredLocation::CODE;
         $neighbour = \Dhl\Versenden\Bcs\Api\Shipment\Service\PreferredNeighbour::CODE;
         if (array_key_exists($location, $services) || array_key_exists($neighbour, $services)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLocationAndNeighbourEnabled()
+    {
+        /**
+         * @var Dhl_Versenden_Model_Config_Service $config
+         */
+        $config = Mage::getModel('dhl_versenden/config_service');
+        $services = $config->getEnabledServices(Mage::app()->getStore()->getStoreId())->getItems();
+        $location = \Dhl\Versenden\Bcs\Api\Shipment\Service\PreferredLocation::CODE;
+        $neighbour = \Dhl\Versenden\Bcs\Api\Shipment\Service\PreferredNeighbour::CODE;
+        if (array_key_exists($location, $services) && array_key_exists($neighbour, $services)) {
             return true;
         }
 
