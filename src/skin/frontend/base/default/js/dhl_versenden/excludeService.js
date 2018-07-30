d833d9fd1d15f43f98d7fa4e793f48cfd71a64e8
Max Melzer <max.melzer@netresearch.de>
2018-07-30 14:31:34 +0200

    DHLGKP-181: Add descriptive placeholders for disabled inputs

--- src/skin/frontend/base/default/js/dhl_versenden/excludeService.js
+++ src/skin/frontend/base/default/js/dhl_versenden/excludeService.js
@@ -1,16 +1,27 @@
-
+/**
+ * When setting a value on one of the input elements, the other will be disabled.
+ *
+ * @param {Element} location
+ * @param {Element} neighbour
+ */
 function observeAndDisable(location, neighbour) {
-    addKeypressListener(location, neighbour);
-    addKeypressListener(neighbour, location);
+    addKeypressListener(location, neighbour, Translator.translate('Not available with preferred location'));
+    addKeypressListener(neighbour, location, Translator.translate('Not available with preferred neighbor'));
 }
 
-function addKeypressListener(elem, sibling) {
+/**
+ *
+ * @param {Element} elem
+ * @param {Element} sibling
+ * @param {string} placeholder
+ */
+function addKeypressListener(elem, sibling, placeholder) {
     var orgPlaceholder = sibling.placeholder;
-    elem.addEventListener('change', function(){
+    elem.addEventListener('input', function(){
         self = elem;
         if (self.value !== '') {
             if (!sibling.disabled) {
-                sibling.placeholder = '--------';
+                sibling.placeholder = placeholder;
                 sibling.disable();
             }
         }
